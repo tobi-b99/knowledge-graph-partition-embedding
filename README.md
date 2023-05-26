d833efcab4287988b4d55644e4b74eda28d08602
Tobias Bode <tobias6199@gmail.com>
2023-05-26 10:25:39 +0200

    Moved to public repository

---
+++ README.md
@@ -0,0 +1,35 @@
+# Knowledge Graph Embedding Partition
+
+The goal of this project is to experiment with partitioning knowledge graphs (KG) before they are embedded and evaluating the performance differences compared to using one big KG.
+
+
+## Dependencies
+
+The project was using Python 3.7.12 with following required depencies:
+- numpy==1.19.1
+- pandas==1.0.5
+- scipy==1.5.1
+- scikit-learn==0.23.1
+- h5py==2.10.0
+- pathlib2 (tested with 2.3.7.post1)
+- requests (tested with 2.28.1)
+- tqdm (tested with 4.64.1)
+- gensim (tested with 3.8.3)
+- aiohttp (tested with 3.8.4)
+- evaluation-framework==2.0
+
+
+### Installing Evaluation-Framework ([GEval](https://github.com/mariaangelapellegrino/Evaluation-Framework))
+
+Since the project is not up to date on [pypi.org](pypi.org) (at the time of this project), either the dependencies have to be downgraded to fit an older version or a newer version can be installed directly from Github for example like this:
+```
+pip install git+https://github.com/mariaangelapellegrino/Evaluation-Framework@master
+```
+### Partitioning
+For partitioning an [adaptation](https://github.com/dice-group/rdf-partitioning) of [Koral](https://github.com/Institute-Web-Science-and-Technologies/koral) by Akther et al. was used to partition and encode the graph into chunks. Then the ChunkTranslator provided by Koral was used to transform the chunks back into decoded graph files. See the READMEs of the corresponding projects for more information.
+
+### jRDF2vec
+[jRdf2Vec](https://github.com/dwslab/jRDF2Vec), an implementation of RDF2vec in Java, was used for embedding the graphs. First the walks were created, then the embeddings for the graphs.
+
+## Dataset
+The dataset that was used is [Cleaned object properties extracted with mappings](https://databus.dbpedia.org/dbpedia/mappings/mappingbased-objects).
---
+++ src/utils.py
@@ -0,0 +1,173 @@
+import bz2
+import glob
+import gzip
+import os
+from os.path import join
+
+import requests
+import tqdm
+from rdflib import Graph
+
+
+def get_all_data(datasets: list = None, path: str = "data"):
+    """Downloads all data and decompress them.
+    
+    Args:
+        datasets (list): list of datasets to get.
+        path (str): path to location where files should be stored.
+
+    Returns:
+        none
+
+    """
+    assert datasets is not None, "No datasets selected, please spefify at least one."
+    assert path is not None, "No path given, please specify one."
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    for link in datasets:
+        file_name = link.rsplit("/", 1)[-1]
+        zip_path = os.path.join(path, file_name)
+        file_path = zip_path.rsplit(".", 1)[0]
+
+        if not os.path.exists(zip_path) and not os.path.exists(file_path):
+            download(link, zip_path)
+
+        if not os.path.exists(file_path):
+            decompress(zip_path)
+
+
+def download(url: str = None, path: str = None):
+    """Downloads files from a link to a path
+    
+    Args:
+        url (str): URL where files can be found.
+        path (str): path to location where files should be stored.
+
+    Returns:
+        none
+
+    """
+    assert url is not None, "No url given, please spefify one."
+    assert path is not None, "No path given, please specify one."
+
+    response = requests.get(url, stream=True)
+    total_bytesize = int(response.headers.get('content-length', 0))
+    progress_bar = tqdm.tqdm(total=total_bytesize, unit='iB', unit_scale=True)
+
+    with open(path, "wb") as file:
+        for data in response.iter_content(8192):
+            progress_bar.update(len(data))
+            file.write(data)
+    progress_bar.close()
+
+    if total_bytesize != 0 and progress_bar.n != total_bytesize:
+        raise FileExistsError
+
+
+def decompress(path: str = None):
+    ''' Decompresses files.
+    
+    Args:
+        path (str): path to file that is to be decompressed.
+        
+    Returns:
+        none
+        
+    '''
+    assert path is not None, "No path given, please specify."
+
+    if path.endswith("bz2"):
+        new_path = os.path.splitext(path)[0]
+        print("Decompressing:", new_path)
+        with open(new_path, "wb") as new_file, bz2.BZ2File(path, "rb") as file:
+            for data in iter(lambda: file.read(100 * 1024), b''):
+                new_file.write(data)
+
+        os.remove(path) # remove archive file to save space
+    elif(path.endswith("gz")):
+        new_path = os.path.splitext(path)[0]
+        print("Decompressing:", new_path)
+        with open(new_path, "wb") as new_file, gzip.open(path, "rb") as file:
+            for data in iter(lambda: file.read(100 * 1024), b''):
+                new_file.write(data)
+
+    else:
+        print("Unexpected filetype ecountered. gz- or bz2-file was expected.")
+
+
+def import_ttl(paths: list = None, g: Graph = None):
+    ''' Combines ttl-files into one (big) ttl-file.
+    
+    Args:
+        paths (list): paths to the ttl files that are to be combined.
+        
+    Returns:
+        none
+        
+    '''
+    assert paths is not None, "No paths given, please give a list with at least one path."
+    assert Graph is not None, "No Graph given, please specify one."
+    
+    print("Importing...")
+    for p in paths:
+        g.parse(p, format="ttl")
+        print(p, "imported.")
+
+
+def combine_vectors(file_regex: str = None, outpath: str = None):
+    ''' Concatenates txt files in a new vectors.txt file.
+    
+    Args:
+        file_regex (str): the path to the files to combine, can contain wildcards.
+        outpath (str): path to a file that the result is to be output in.
+        
+    Returns:
+        none
+        
+    '''
+    assert file_regex is not None, "No path to the files given, please specify."
+    assert outpath is not None, "No path to output files given, please specify."
+
+    output_folder = os.path.dirname(outpath)
+    print
+    if not os.path.exists(output_folder):
+        os.mkdir(output_folder)
+    
+    with open(outpath, "w") as outfile:
+        for file in glob.glob(file_regex):
+            with open(file, "r") as infile:
+                for line in infile:
+                    outfile.write(line)
+
+
+def write_embedded_file(file_type: str = "txt", file_name: str = None, entities: list = None, embeddings: list = None):
+    ''' Writes embeddings and entities in a file in a way that GEval can read.
+    
+    Args:
+        file_type (str): the file type to be used. Standard is txt.
+        file_name (str): the file name that should be used. Will be written to models folder.
+        entities (list): the IRIs or names of the embedded entities.
+        embeddings (list): the embeddings that should be written in the file.
+        
+    Returns:
+        none
+        
+    '''
+
+    assert file_name is not None, "No file name given, please specify."
+    assert entities is not None, "No entities given, please give an array containing at least one."
+    assert embeddings is not None, "No embedding vectors given, please give an array containing at least one."
+
+    if(file_type == "txt"):
+        file_type = ".txt"
+    else: #only allow intended file types
+        return
+
+    with open(join("models", file_name + file_type), "w") as f:
+        for t, e in zip(entities, embeddings):
+            f.write(t)
+            for v in e:
+                f.write(" {}".format(v))
+            f.write("\n")
